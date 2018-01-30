58afb72cf45e30dca30fdf50dd89ed5e769704ae
Deivis <deiviswingert@hotmail.com>
2018-01-30 15:32:32 -0200

    icons: add sidebar icons

--- src/icons/index.js
+++ src/icons/index.js
@@ -2,10 +2,12 @@ const UIAvatar = require('./avatar')
 const UICard = require('./card')
 const UIDateInput = require('./dateInput')
 const UITable = require('./table')
+const UISidebar = require('./sidebar')
 
 module.exports = {
   UIAvatar,
   UICard,
   UIDateInput,
   UITable,
+  UISidebar,
 }
---
+++ src/icons/sidebar/index.js
@@ -0,0 +1,8 @@
+const React = require('react')
+const IconArrowDown = require('react-icons/lib/md/keyboard-arrow-down')
+const IconArrowUp = require('react-icons/lib/md/keyboard-arrow-up')
+
+module.exports = {
+  collapse: <IconArrowUp />,
+  expand: <IconArrowDown />,
+}
