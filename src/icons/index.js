d6953776f238e9e3890f3f79dd7e68d4815c3e64
Deivis <deiviswingert@hotmail.com>
2018-01-30 16:07:19 -0200

    icons: fix vars order

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,15 +1,12 @@
 const UIAvatar = require('./avatar')
 const UICard = require('./card')
 const UIDateInput = require('./date-input')
-<<<<<<< HEAD
+const UIDateSelector = require('./date-selector')
 const UIDropdown = require('./dropdown')
 const UIDropdownForm = require('./form-fields/dropdown')
 const UIHeader = require('./header')
 const UIInput = require('./input')
 const UIInputForm = require('./form-fields/input')
-=======
-const UIDateSelector = require('./date-selector')
->>>>>>> a3cd435e998af2b807c560972b9367e1970b5d0e
 const UIPagination = require('./pagination')
 const UISidebar = require('./sidebar')
 const UITable = require('./table')
@@ -18,15 +15,12 @@ module.exports = {
   UIAvatar,
   UICard,
   UIDateInput,
-<<<<<<< HEAD
+  UIDateSelector,
   UIDropdown,
   UIDropdownForm,
   UIHeader,
   UIInput,
   UIInputForm,
-=======
-  UIDateSelector,
->>>>>>> a3cd435e998af2b807c560972b9367e1970b5d0e
   UIPagination,
   UISidebar,
   UITable,
