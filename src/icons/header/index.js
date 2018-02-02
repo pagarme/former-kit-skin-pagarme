99869cb12c69470ca701e802db245ae98ec531f6
Leonardo Felipe Nerone <leonardofelipenerone@gmail.com>
2018-02-02 19:18:56 -0200

    header: add header-menu's expand icon

--- src/icons/header/index.js
+++ src/icons/header/index.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import BackArrow from 'emblematic-icons/svg/ChevronBack24.svg'
+import DownArrow from 'emblematic-icons/svg/ChevronDown24.svg'
 
 module.exports = {
   back: <BackArrow width="12px" height="12px" />,
+  expand: <DownArrow width="12px" height="12px" />,
 }
