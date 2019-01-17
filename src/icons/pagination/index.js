d5ad0b527c20deed78caba9f13882e822bea34ca
Deivis <deiviswingert@hotmail.com>
2019-01-17 14:51:55 -0200

    chore: refactor lint errors
    Refactor lint errors and incompatibility
    with upgraded dependencies interfaces

--- src/icons/pagination/index.js
+++ src/icons/pagination/index.js
@@ -3,6 +3,6 @@ import BackArrow from 'emblematic-icons/svg/ChevronBack24.svg'
 import ForwardArrow from 'emblematic-icons/svg/ChevronForward24.svg'
 
 module.exports = {
-  previous: <BackArrow width="12px" height="12px" />,
   next: <ForwardArrow width="12px" height="12px" />,
+  previous: <BackArrow width="12px" height="12px" />,
 }
--- src/icons/table/index.js
+++ src/icons/table/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import IconArrowDown from 'emblematic-icons/svg/ChevronDown24.svg'
 import IconArrowUp from 'emblematic-icons/svg/ChevronUp24.svg'
-import IconSort from 'emblematic-icons/svg/OrderTable24.svg'
+import IconSort from 'emblematic-icons/svg/Sort24.svg'
 
 module.exports = {
-  expand: <IconArrowDown width="12px" height="12px" />,
+  ascending: <IconArrowUp width="12px" height="12px" />,
   collapse: <IconArrowUp width="12px" height="12px" />,
   descending: <IconArrowDown width="12px" height="12px" />,
-  ascending: <IconArrowUp width="12px" height="12px" />,
+  expand: <IconArrowDown width="12px" height="12px" />,
   orderable: <IconSort width="12px" height="12px" />,
 }
