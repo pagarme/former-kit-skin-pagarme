05827a89f8f3ba86fe2db18cde8967431c435f98
Zignd <zignd.igor@gmail.com>
2020-08-03 13:44:30 -0300

    Revert "icons: update sizes for password inputs"
    This reverts commit d468312c15ec97b1b07fc68f79cf0a07d29144c6.

--- src/icons/input/index.js
+++ src/icons/input/index.js
@@ -3,6 +3,6 @@ import HideIcon from 'emblematic-icons/svg/Hide24.svg'
 import ShowIcon from 'emblematic-icons/svg/Show24.svg'
 
 module.exports = {
-  hidePassword: <HideIcon />,
-  showPassword: <ShowIcon />,
+  hidePassword: <HideIcon width="12px" height="12px" />,
+  showPassword: <ShowIcon width="12px" height="12px" />,
 }
